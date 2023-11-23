7eac299508e6bc808dcb8c3ef37f19f52f2cbe44
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-24 07:24:53 +0900

    feat: decode command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use anyhow::Context;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Decode { value: String },
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Decode { value } => {
+            let decoded: String = serde_bencode::from_str(&value).context("decode value")?;
+            println!("{decoded}");
+        }
+    }
+
+    Ok(())
 }
