5c885229bc72724cec334eced9449fd5c7046a0b
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-26 16:18:13 +0900

    doc: paste spec of connect request and response

--- src/tracker/udp.rs
+++ src/tracker/udp.rs
@@ -1,6 +1,6 @@
-use std::io::{self, Write};
+use std::io::{self, Cursor, Write};
 
-use byteorder::{NetworkEndian, WriteBytesExt};
+use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
 use serde::Deserialize;
 
 const PROTOCOL_IDENTIFIER: u64 = 0x0417_2710_1980;
@@ -11,6 +11,11 @@ pub struct TransactionId(pub u32);
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug, Deserialize)]
 pub struct ConnectionId(pub u64);
 
+/// Offset  Size            Name            Value
+/// 0       64-bit integer  protocol_id     0x41727101980 // magic constant
+/// 8       32-bit integer  action          0 // connect
+/// 12      32-bit integer  transaction_id
+/// 16
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct ConnectRequest {
     pub transaction_id: TransactionId,
@@ -41,6 +46,11 @@ impl Request {
     }
 }
 
+/// Offset  Size            Name            Value
+/// 0       32-bit integer  action          0 // connect
+/// 4       32-bit integer  transaction_id
+/// 8       64-bit integer  connection_id
+/// 16
 #[derive(PartialEq, Eq, Clone, Debug, Deserialize)]
 pub struct ConnectResponse {
     pub connection_id: ConnectionId,
@@ -54,16 +64,14 @@ pub enum Response {
 
 impl Response {
     pub fn read(bytes: &[u8]) -> Result<Self, io::Error> {
-        // let action = u32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
-        let action = u32::from_be_bytes(bytes[0..4].try_into().unwrap());
+        let mut cursor = Cursor::new(bytes);
+        let action = cursor.read_u32::<NetworkEndian>()?;
 
         match action {
             // Connect
             0 => {
-                let connection_id =
-                    ConnectionId(u64::from_be_bytes(bytes[8..16].try_into().unwrap()));
-                let transaction_id =
-                    TransactionId(u32::from_be_bytes(bytes[4..8].try_into().unwrap()));
+                let transaction_id = TransactionId(cursor.read_u32::<NetworkEndian>()?);
+                let connection_id = ConnectionId(cursor.read_u64::<NetworkEndian>()?);
 
                 Ok(Self::Connect(ConnectResponse {
                     connection_id,
