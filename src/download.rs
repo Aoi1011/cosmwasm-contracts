64816dd8e34fc0d522a47e3d752b292e72454fc0
aoikurokawa <ht.so.ik0102@gmail.com>
2023-12-13 08:32:41 +0900

    feat: update

--- src/download.rs
+++ src/download.rs
@@ -1,9 +1,7 @@
-use std::{collections::BinaryHeap, time::Duration};
+use std::collections::BinaryHeap;
 
-use anyhow::{anyhow, Context};
 use futures_util::StreamExt;
 use sha1::{Digest, Sha1};
-use tokio::net::UdpSocket;
 
 use crate::{
     block::BLOCK_SIZE,
@@ -15,303 +13,151 @@ use crate::{
 
 pub async fn all(t: &Torrent) -> anyhow::Result<Downloaded> {
     let info_hash = t.info_hash();
+    let req = tracker::http::Request::new(&info_hash, t.length());
+    let url = req.url(&t.announce);
 
-    let addr = tracker::get_addr(&t.announce)?;
+    let res = reqwest::get(url).await?;
+    let res_bytes = res.bytes().await?;
+    let tracker_res: tracker::http::Response = serde_bencode::from_bytes(&res_bytes)?;
 
-    match addr {
-        tracker::Addr::Udp(url) => {
-            let socket = UdpSocket::bind("0.0.0.0:0")
-                .await
-                .context("bind to the address")?;
-            socket.connect(url).await.context("connect to tracker")?;
+    let mut peers = futures_util::stream::iter(tracker_res.peers.0)
+        .map(|peer_addr| async move {
+            let peer = Peer::new(peer_addr, &info_hash).await;
+            (peer_addr, peer)
+        })
+        .buffer_unordered(5);
 
-            let mut action = 0;
-            let mut transaction_id = 0;
-            let mut connection_id: u64 = 0;
+    let mut peer_list = Vec::new();
+    while let Some((peer_addr, peer)) = peers.next().await {
+        match peer {
+            Ok(peer) => {
+                peer_list.push(peer);
 
-            'transmit: loop {
-                match action {
-                    // Connect
-                    0 => {
-                        let mut connect_buffer = Vec::new();
-                        transaction_id = rand::random::<u32>();
-                        let connect_req = tracker::udp::ConnectRequest::new(transaction_id);
-                        let request = tracker::udp::Request::from(connect_req);
-                        request.write(&mut connect_buffer)?;
-
-                        let mut attempts = 0;
-                        let max_retries = 8;
-                        let mut delay = 15;
-                        loop {
-                            eprintln!("attempting to send request: {}", attempts);
-
-                            if attempts > max_retries {
-                                return Err(anyhow!("max retransmission reached"));
-                            }
-                            // Send the connect request
-                            match socket.send_to(&connect_buffer, &url).await {
-                                Ok(_) => break,
-                                Err(e) => {
-                                    println!(
-                                        "attempt {}: Failed to send request, error: {}",
-                                        attempts, e
-                                    );
-                                }
-                            }
-
-                            tokio::time::sleep(Duration::from_secs(delay)).await;
-
-                            attempts += 1;
-
-                            delay *= 2;
-                        }
-                    }
-
-                    // Announce
-                    1 => {
-                        let mut announce_buffer = Vec::new();
-                        transaction_id = rand::random::<u32>();
-                        let announce_req = tracker::udp::AnnounceRequest::new(
-                            connection_id,
-                            transaction_id,
-                            t.info_hash(),
-                        );
-                        let request = tracker::udp::Request::from(announce_req);
-                        request.write(&mut announce_buffer)?;
-
-                        let mut attempts = 0;
-                        let max_retries = 8;
-                        let mut delay = 15;
-                        loop {
-                            eprintln!("attempting to send request: {}", attempts);
-
-                            if attempts > max_retries {
-                                return Err(anyhow!("max retransmission reached"));
-                            }
-                            // Send the connect request
-                            match socket.send_to(&announce_buffer, &url).await {
-                                Ok(_) => break,
-                                Err(e) => {
-                                    println!(
-                                        "attempt {}: Failed to send request, error: {}",
-                                        attempts, e
-                                    );
-                                }
-                            }
-
-                            tokio::time::sleep(Duration::from_secs(delay)).await;
-
-                            attempts += 1;
-
-                            delay *= 2;
-                        }
-                    }
-                    _ => {}
-                }
-
-                // Buffer to receive the response
-                let mut response: Vec<u8> = vec![0; 1206];
-
-                // Receive the response
-                match socket.recv(&mut response).await {
-                    Ok(_) => {
-                        let res =
-                            tracker::udp::Response::read(&mut response).context("read response")?;
-
-                        // Check if the transaction_id matches
-                        match res {
-                            tracker::udp::Response::Connect(connect_res) => {
-                                assert_eq!(connect_res.transaction_id.0, transaction_id);
-
-                                println!("Received connection ID: {}", connect_res.connection_id.0);
-
-                                action = 1;
-                                connection_id = connect_res.connection_id.0;
-                            }
-                            tracker::udp::Response::Announce(announce_res) => {
-                                assert_eq!(announce_res.transaction_id.0, transaction_id);
-
-                                eprintln!("Peers");
-                                for (idx, peer) in announce_res.peers.iter().enumerate() {
-                                    eprintln!("Peer {idx}: {peer}");
-                                }
-
-                                break 'transmit;
-                            }
-                            tracker::udp::Response::Scrape(scrape_res) => {}
-                            tracker::udp::Response::Error(err_res) => {}
-                        }
-                    }
-                    Err(e) => {
-                        eprintln!("Failed to receive response: {:?}", e);
-                    }
-                }
-
-                return Ok(Downloaded {
-                    bytes: all_pieces,
-                    files: match &t.info.keys {
-                        Keys::SingleFile { length } => vec![File {
-                            length: *length,
-                            path: vec![t.info.name.clone()],
-                        }],
-                        Keys::MultiFile { files } => files.clone(),
-                    },
-                });
-            }
-        }
-        tracker::Addr::Http(url) => {
-            let res = reqwest::get(request.url(&url.to_string())).await?;
-            let res: tracker::http::Response =
-                serde_bencode::from_bytes(&res.bytes().await?).context("parse response")?;
-
-            for peer in res.peers.0 {
-                println!("{peer}");
-            }
-            let res = reqwest::get(url).await?;
-            let res_bytes = res.bytes().await?;
-            let tracker_res: tracker::http::Response = serde_bencode::from_bytes(&res_bytes)?;
-
-            let mut peers = futures_util::stream::iter(tracker_res.peers.0)
-                .map(|peer_addr| async move {
-                    let peer = Peer::new(peer_addr, &info_hash).await;
-                    (peer_addr, peer)
-                })
-                .buffer_unordered(5);
-
-            let mut peer_list = Vec::new();
-            while let Some((peer_addr, peer)) = peers.next().await {
-                match peer {
-                    Ok(peer) => {
-                        peer_list.push(peer);
-
-                        if peer_list.len() > 5 {
-                            break;
-                        }
-                    }
-                    Err(e) => {
-                        eprintln!("connect to peer {peer_addr:?}: {e}");
-                    }
+                if peer_list.len() > 5 {
+                    break;
                 }
             }
-            drop(peers);
-
-            let mut peers = peer_list;
-            let mut need_pieces = BinaryHeap::new();
-            let mut no_peers = Vec::new();
-
-            for piece_i in 0..t.info.pieces.0.len() {
-                let piece = Piece::new(piece_i, &t, &peers);
-                if piece.peers().is_empty() {
-                    no_peers.push(piece);
-                } else {
-                    need_pieces.push(piece);
-                }
+            Err(e) => {
+                eprintln!("connect to peer {peer_addr:?}: {e}");
             }
-
-            assert!(no_peers.is_empty());
-
-            let mut all_pieces = vec![0; t.length()];
-            while let Some(piece) = need_pieces.pop() {
-                let plength = piece.length();
-                let npiece = piece.index();
-                let piece_length = plength.min(t.length() - plength * npiece);
-                let total_blocks = if piece_length % BLOCK_SIZE as usize == 0 {
-                    piece_length / BLOCK_SIZE as usize
-                } else {
-                    (piece_length / BLOCK_SIZE as usize) + 1
-                };
-
-                let peers: Vec<_> = peers
-                    .iter_mut()
-                    .enumerate()
-                    .filter_map(|(peer_i, peer)| piece.peers().contains(&peer_i).then_some(peer))
-                    .collect();
-
-                let (submit, tasks) = kanal::bounded_async(total_blocks);
-                for block in 0..total_blocks {
-                    submit
-                        .send(block)
-                        .await
-                        .expect("bound holds all these limits");
-                }
-
-                let (finish, mut done) = tokio::sync::mpsc::channel(total_blocks);
-                let mut participants = futures_util::stream::FuturesUnordered::new();
-                for peer in peers {
-                    participants.push(peer.participate(
-                        piece.index() as u32,
-                        total_blocks as u32,
-                        piece_length as u32,
-                        submit.clone(),
-                        tasks.clone(),
-                        finish.clone(),
-                    ));
-                }
-                drop(submit);
-                drop(finish);
-                drop(tasks);
-
-                let mut all_blocks: Vec<u8> = vec![0; piece_length];
-                let mut bytes_received = 0;
-                loop {
-                    tokio::select! {
-                        joined = participants.next(), if !participants.is_empty() => {
-                            // if a participant ends early, it's either slow or failed.
-                            match joined {
-                                None => {},
-                                Some(Ok(_)) => {},
-                                Some(Err(_)) => {},
-                            }
-                        },
-
-                        piece = done.recv() => {
-                        // keep track of the bytes in message
-                            if let Some(piece) = piece {
-                                // let piece = Piece::ref_from_bytes(&piece.block()[..]).expect("always get all Piece response fields from peer");
-                                all_blocks[piece.begin() as usize ..][..piece.block().len()].copy_from_slice(piece.block());
-                                bytes_received += piece.block().len();
-                                if bytes_received ==  piece_length {
-                                    break;
-                                }
-                            } else {
-                                break;
-                            }
-
-                        },
-                    }
-                }
-                drop(participants);
-
-                if bytes_received == piece_length {
-                    // great, we got all the bytes
-                } else {
-                    // we'll need to connect to more peers, and make sure that those additional peers also
-                    // have this piece, and then download the piece we _didn't_ get from them.
-                    // probably also stick this back onto the pices_heap
-                    anyhow::bail!("no peers left to get piece {}", piece.index());
-                }
-
-                let mut hasher = Sha1::new();
-                hasher.update(&all_blocks);
-                let hash: [u8; 20] = hasher.finalize().try_into().expect("");
-                assert_eq!(hash, piece.hash());
-
-                all_pieces[piece.index() * t.info.plength..][..piece_length]
-                    .copy_from_slice(&all_blocks);
-            }
-
-            Ok(Downloaded {
-                bytes: all_pieces,
-                files: match &t.info.keys {
-                    Keys::SingleFile { length } => vec![File {
-                        length: *length,
-                        path: vec![t.info.name.clone()],
-                    }],
-                    Keys::MultiFile { files } => files.clone(),
-                },
-            })
         }
     }
+    drop(peers);
+
+    let mut peers = peer_list;
+    let mut need_pieces = BinaryHeap::new();
+    let mut no_peers = Vec::new();
+
+    for piece_i in 0..t.info.pieces.0.len() {
+        let piece = Piece::new(piece_i, &t, &peers);
+        if piece.peers().is_empty() {
+            no_peers.push(piece);
+        } else {
+            need_pieces.push(piece);
+        }
+    }
+
+    assert!(no_peers.is_empty());
+
+    let mut all_pieces = vec![0; t.length()];
+    while let Some(piece) = need_pieces.pop() {
+        let plength = piece.length();
+        let npiece = piece.index();
+        let piece_length = plength.min(t.length() - plength * npiece);
+        let total_blocks = if piece_length % BLOCK_SIZE as usize == 0 {
+            piece_length / BLOCK_SIZE as usize
+        } else {
+            (piece_length / BLOCK_SIZE as usize) + 1
+        };
+
+        let peers: Vec<_> = peers
+            .iter_mut()
+            .enumerate()
+            .filter_map(|(peer_i, peer)| piece.peers().contains(&peer_i).then_some(peer))
+            .collect();
+
+        let (submit, tasks) = kanal::bounded_async(total_blocks);
+        for block in 0..total_blocks {
+            submit
+                .send(block)
+                .await
+                .expect("bound holds all these limits");
+        }
+
+        let (finish, mut done) = tokio::sync::mpsc::channel(total_blocks);
+        let mut participants = futures_util::stream::FuturesUnordered::new();
+        for peer in peers {
+            participants.push(peer.participate(
+                piece.index() as u32,
+                total_blocks as u32,
+                piece_length as u32,
+                submit.clone(),
+                tasks.clone(),
+                finish.clone(),
+            ));
+        }
+        drop(submit);
+        drop(finish);
+        drop(tasks);
+
+        let mut all_blocks: Vec<u8> = vec![0; piece_length];
+        let mut bytes_received = 0;
+        loop {
+            tokio::select! {
+                joined = participants.next(), if !participants.is_empty() => {
+                    // if a participant ends early, it's either slow or failed.
+                    match joined {
+                        None => {},
+                        Some(Ok(_)) => {},
+                        Some(Err(_)) => {},
+                    }
+                },
+
+                piece = done.recv() => {
+                // keep track of the bytes in message
+                    if let Some(piece) = piece {
+                        // let piece = Piece::ref_from_bytes(&piece.block()[..]).expect("always get all Piece response fields from peer");
+                        all_blocks[piece.begin() as usize ..][..piece.block().len()].copy_from_slice(piece.block());
+                        bytes_received += piece.block().len();
+                        if bytes_received ==  piece_length {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+
+                },
+            }
+        }
+        drop(participants);
+
+        if bytes_received == piece_length {
+            // great, we got all the bytes
+        } else {
+            // we'll need to connect to more peers, and make sure that those additional peers also
+            // have this piece, and then download the piece we _didn't_ get from them.
+            // probably also stick this back onto the pices_heap
+            anyhow::bail!("no peers left to get piece {}", piece.index());
+        }
+
+        let mut hasher = Sha1::new();
+        hasher.update(&all_blocks);
+        let hash: [u8; 20] = hasher.finalize().try_into().expect("");
+        assert_eq!(hash, piece.hash());
+
+        all_pieces[piece.index() * t.info.plength..][..piece_length].copy_from_slice(&all_blocks);
+    }
+
+    Ok(Downloaded {
+        bytes: all_pieces,
+        files: match &t.info.keys {
+            Keys::SingleFile { length } => vec![File {
+                length: *length,
+                path: vec![t.info.name.clone()],
+            }],
+            Keys::MultiFile { files } => files.clone(),
+        },
+    })
 }
 
 pub struct Downloaded {
